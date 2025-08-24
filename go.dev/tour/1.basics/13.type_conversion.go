1ad165f767bcaddba12d539021798306257c52c4
Muhammad Rafid Amrullah <rafid.tijarah@gmail.com>
2025-08-24 11:20:37 +0700

    go.dev/tour/basics: update type conversion examples

--- go.dev/tour/1.basics/13.type_conversion.go
+++ go.dev/tour/1.basics/13.type_conversion.go
@@ -2,15 +2,15 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func type_conversion() {
 	var x, y int = 3, 4
-	// var f float64 = math.Sqrt(float64(x*x + y*y))
-	var f float64 = float64(x*x + y*y)
-	var z1 uint = uint(f)
+	var f float64 = math.Sqrt(float64(x*x + y*y))
+	var z1 uint8 = uint8(f * 100000)
 	var z2 uint = -uint(f)
 	var z3 uint = uint(-f)
-	fmt.Println(f, z1, z2, z3)
+	fmt.Printf("f=%v z1=%v z2=%v z3=%v\n", f, z1, z2, z3)
 	fmt.Printf("z2 == z3: %v\n", z2 == z3)
 }
