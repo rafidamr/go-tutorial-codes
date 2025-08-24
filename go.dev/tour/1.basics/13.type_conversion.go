485ee9b550c4b8cc333055fd80c36ec9b3e74c66
Muhammad Rafid Amrullah <rafid.tijarah@gmail.com>
2025-08-24 13:43:07 +0700

    go.dev/tour/basics: add untyped constant

--- go.dev/tour/1.basics/13.type_conversion.go
+++ go.dev/tour/1.basics/13.type_conversion.go
@@ -5,6 +5,10 @@ import (
 	"math"
 )
 
+func floatContext(x float64) float64 {
+	return x
+}
+
 func type_conversion() {
 	var x, y int = 3, 4
 	var f float64 = math.Sqrt(float64(x*x + y*y))
@@ -13,4 +17,7 @@ func type_conversion() {
 	var z3 uint = uint(-f)
 	fmt.Printf("f=%v z1=%v z2=%v z3=%v\n", f, z1, z2, z3)
 	fmt.Printf("z2 == z3: %v\n", z2 == z3)
+
+	const Big = 1 << 100
+	fmt.Printf("%v\n", floatContext(Big))
 }
