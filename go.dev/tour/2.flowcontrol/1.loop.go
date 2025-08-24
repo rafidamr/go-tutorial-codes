824a3bb8548d2bdbd639ba3c45c1c726a9351cbe
Muhammad Rafid Amrullah <rafid.tijarah@gmail.com>
2025-08-24 15:12:39 +0700

    go.dev/tour/flowcontrol: add example looping and conditional

---
+++ go.dev/tour/2.flowcontrol/1.loop.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func looping() {
+	var sum int
+	for i := 0; sum < 1000; i++ {
+		sum++
+	}
+	fmt.Println(sum)
+	sum = 0
+	for sum < 1000 { // Similar to while
+		sum++
+	}
+	fmt.Println(sum)
+	fmt.Println(fmt.Sprint(math.Sqrt(-(-float64(sum)))) + "i")
+
+	if x := math.Sqrt(70); x < 6 {
+		fmt.Println(x)
+	} else {
+		fmt.Printf("%g >= %g\n", x, float64(6))
+	}
+}
---
+++ go.dev/tour/2.flowcontrol/go.mod
@@ -0,0 +1,3 @@
+module 2.flowcontrol
+
+go 1.25.0
