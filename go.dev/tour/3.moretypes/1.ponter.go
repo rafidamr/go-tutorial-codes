76124961738011376a9336ed14b79904fc4b440a
Muhammad Rafid Amrullah <rafid.tijarah@gmail.com>
2025-08-26 12:40:55 +0700

    go.dev/tour/moretypes: add more example in pointer

--- go.dev/tour/3.moretypes/1.ponter.go
+++ go.dev/tour/3.moretypes/1.ponter.go
@@ -3,11 +3,15 @@ package main
 import "fmt"
 
 func pointer() {
-	var p *int
-	var i = 123
+	var p *float32
+	var i, j float32 = 123, 9
 	p = &i
 	fmt.Println(p)
 	fmt.Println(*p)
 	*p += 321
 	fmt.Println(i)
+
+	p = &j
+	*p = *p / 2
+	fmt.Println(j)
 }
