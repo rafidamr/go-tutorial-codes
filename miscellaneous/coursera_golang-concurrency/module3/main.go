a4eedb56b5fa2d651058bcccdacfa4a8f0b54c58
Muhammad Rafid Amrullah <rafid.tijarah@gmail.com>
2025-09-09 23:28:55 +0700

    go.dev/tour/miscellaneous: add synchorization using WaaitGroup

--- miscellaneous/coursera_golang-concurrency/module3/main.go
+++ miscellaneous/coursera_golang-concurrency/module3/main.go
@@ -2,11 +2,30 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
-func main() {
+func f2go(wg *sync.WaitGroup, str string) {
+	fmt.Println(str)
+	wg.Done()
+}
+
+func f2() {
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go f2go(&wg, "task1")
+	go f2go(&wg, "task2")
+	wg.Wait()
+	fmt.Println("Main goroutine")
+}
+
+func f1() {
 	go fmt.Println("New goroutine")
 	fmt.Println("Main goroutine")
 	time.Sleep(1000 * time.Millisecond)
+}
+
+func main() {
+	f2()
 }
